b17d59b6064f575bf8f489c976959349cf08dacb
Macbook Pro <truongduchung94@gmail.com>
2021-09-29 21:37:40 +0700

    commit

--- spring-boot-security-jwt/src/main/java/com/bezkoder/springjwt/security/WebSecurityConfig.java
+++ spring-boot-security-jwt/src/main/java/com/bezkoder/springjwt/security/WebSecurityConfig.java
@@ -60,7 +60,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
 			.antMatchers("/api/test/**").permitAll()
 			.antMatchers("/api/tutorial/**").permitAll()
-			
+			.antMatchers("/**").permitAll()
 			.anyRequest().authenticated();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
