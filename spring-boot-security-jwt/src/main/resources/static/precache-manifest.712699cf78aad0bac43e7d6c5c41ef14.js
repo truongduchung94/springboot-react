28ad998443b3b4dc52cc3b0a73183a9912f6ae43
Macbook Pro <truongduchung94@gmail.com>
2021-09-29 16:13:25 +0700

    commit react

---
+++ spring-boot-security-jwt/src/main/resources/static/precache-manifest.712699cf78aad0bac43e7d6c5c41ef14.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d92a188c35540cc04a958eae1d2f80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94a6978dfe0206e50640",
+    "url": "/static/css/2.f2a219a3.chunk.css"
+  },
+  {
+    "revision": "664830566ec8ea0ae8d1",
+    "url": "/static/css/main.e1d704c0.chunk.css"
+  },
+  {
+    "revision": "94a6978dfe0206e50640",
+    "url": "/static/js/2.726e0eac.chunk.js"
+  },
+  {
+    "revision": "b2e167c884a23672a8510d6182ce03fc",
+    "url": "/static/js/2.726e0eac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "664830566ec8ea0ae8d1",
+    "url": "/static/js/main.195baa60.chunk.js"
+  },
+  {
+    "revision": "44aee220bccf4ace3598",
+    "url": "/static/js/runtime-main.d4e61eff.js"
+  },
+  {
+    "revision": "7fb23f3b56a834d9dcf615b5e7c78da8",
+    "url": "/static/media/bootstrap-icons.7fb23f3b.woff"
+  },
+  {
+    "revision": "e07b538aa51b6fa77f32828af21cb591",
+    "url": "/static/media/bootstrap-icons.e07b538a.woff2"
+  }
+]);
